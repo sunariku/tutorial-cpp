cf5608ce740595dc49a9827b2b3432829d2368c7
Made Subali <os.madesubali@gmail.com>
2025-06-26 14:33:17 +0800

    Program Sederhana.

---
+++ pertemuan_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {\
+  int bilA, bilB, hasilPenjumlahan;
+
+  bilA = 10;
+  bilB = 5;
+
+  hasilPenjumlahan = bilA + bilB;
+
+  cout << "Hasil Penjumlahan " << bilA << " + " << bilB << " = " << hasilPenjumlahan;
+
+  return 0;
+}
