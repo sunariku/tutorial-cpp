2f4b51c0f3b983baf8f9eda8d26bde64c2b35ae5
Made Subali <os.madesubali@gmail.com>
2025-07-01 14:14:13 +0800

    Pertemuan 3 Materi Variabel.

---
+++ pertemuan_3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  /*
+  Deklarasi dan inisialisasi nilai ke variabel.
+  Dalam kode berikut terdapat empat variabel, sebagai salah satu contoh:
+
+  Untuk variabelnilaiSiswa1 adalah nama variabel atau identifier, int adalah tipe datanya dan 70 adalah nilainya.
+  */
+
+  int nilaiSiswa1 = 70;
+  int nilaiSiswa2 = 60;
+  int nilaiSiswa3 = 80;
+  float rerataNilaiSiswa = 0.0;
+
+  rerataNilaiSiswa = (nilaiSiswa1 + nilaiSiswa2 + nilaiSiswa3) / 3; // Ekpresi perhitungan nilai rerata nilai tiga siswa
+
+  cout << "Rerata Nilai Siswa: " << rerataNilaiSiswa;
+
+  return 0;
+}
